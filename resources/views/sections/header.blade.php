c332fba07ca3c4d5cd99514875676b80f2ce9f4b
Mark Dionnie Bulingit <dionnie_bulingit@yahoo.com>
2025-04-28 20:14:32 +0800

    up

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -1,53 +1,55 @@
 <header class="banner">
-  <div class="top-message">
-    <span>
-      +88 123 456 789
+  <div style="z-index:1; position: static; width:100vw">
+    <div class="top-message">
+      <span>
+        +88 123 456 789
 
-    </span>
-    <span>
+      </span>
+      <span>
 
-      info@example.com
+        info@example.com
 
-    </span>
-    <span>
+      </span>
+      <span>
 
-      Mon - Fri : 09:00 - 18:00
-    </span>
-  </div>
-  @if (has_nav_menu('primary_navigation'))
-  <nav
-    aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
-    <div class="container">
+        Mon - Fri : 09:00 - 18:00
+      </span>
+    </div>
+    @if (has_nav_menu('primary_navigation'))
+    <nav
+      aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
+      <div class="container">
 
-      <?php
-      $custom_logo_id = get_theme_mod('custom_logo');
-      $logo = wp_get_attachment_image_src($custom_logo_id, 'full');
-      if (has_custom_logo()) {
-        echo '<img width="200px" src="' . esc_url($logo[0]) . '" alt="' . get_bloginfo('name') . '">';
-      } else {
-        echo '<h1 class="site-name">' . get_bloginfo('name') . '</h1>';
-      }
-      ?>
+        <?php
+        $custom_logo_id = get_theme_mod('custom_logo');
+        $logo = wp_get_attachment_image_src($custom_logo_id, 'full');
+        if (has_custom_logo()) {
+          echo '<img width="200px" src="' . esc_url($logo[0]) . '" alt="' . get_bloginfo('name') . '">';
+        } else {
+          echo '<h1 class="site-name">' . get_bloginfo('name') . '</h1>';
+        }
+        ?>
 
-      <div class="primary-navigation-wrapper">
-        @if (has_nav_menu('primary_navigation')) {!!
+        <div class="primary-navigation-wrapper">
+          @if (has_nav_menu('primary_navigation')) {!!
 
-        wp_nav_menu([
-        'theme_location' => 'primary_navigation',
-        'menu_class' => 'primary-navigation ',
-        'container' => false,
-        ])
+          wp_nav_menu([
+          'theme_location' => 'primary_navigation',
+          'menu_class' => 'primary-navigation ',
+          'container' => false,
+          ])
 
-        !!} @endif
-      </div>
+          !!} @endif
+        </div>
 
-      <!--      @if (has_nav_menu('primary_navigation'))
+        <!--      @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu($primarymenu()) !!}
         @endif -->
-    </div>
-  </nav>
-  @endif
+      </div>
+    </nav>
+    @endif
 
 
-
+  </div>
+  <div class="shadow-dummy">&nbsp;</div>
 </header>
